db4d23b25f164acf955905ce2741f86cceca3973
JackMills <JackMills@NotRealEmail.com>
2026-05-24 15:35:08 -0400

    wip

--- worlds/crossword/Options.py
+++ worlds/crossword/Options.py
@@ -1,18 +1,30 @@
 from dataclasses import dataclass
 
-from Options import PerGameCommonOptions
+from Options import FreeText, NumericOption, PerGameCommonOptions
+
+class FilePath(FreeText):
+    pass
+
+class LetterAllocPercent(NumericOption):
+    default = 100
+
+class StartingPercent(NumericOption):
+    default = 10
+
+class ClueAllocPercent(NumericOption):
+    default = 110
 
 
 @dataclass
 class CrosswordOptions(PerGameCommonOptions):
-    puz_file_path: str
+    puz_file_path: FilePath
     puz_file_contents: str
     # Can be over 100%
-    cross_letter_alloc_percent: float = 100
+    cross_letter_alloc_percent: LetterAllocPercent
     # How many clues at game start
-    starting_percent: float = 10
+    starting_percent: StartingPercent
     # Only need to collect 9/11ths of clue items to have all clues unlocked
-    clue_alloc_percent: float = 110
+    clue_alloc_percent: ClueAllocPercent
     
 
 
