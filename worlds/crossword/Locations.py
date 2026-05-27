8aeb1195b0f076ebe9a7f6db5fe1b4807b3fc748
JackMills <JackMills@NotRealEmail.com>
2026-05-27 16:16:51 -0400

    dumb typo

--- worlds/crossword/Locations.py
+++ worlds/crossword/Locations.py
@@ -10,6 +10,7 @@ class CrosswordLocation(Location):
         super().__init__(player, name, address, parent)
 
 MAX_N_CLUES = 1000
+LOCATION_OFFSET = MAX_N_CLUES * 10;
 
 
 
@@ -17,10 +18,10 @@ def get_location_name(clue_id: int, direction: Direction):
     return f"Solved Clue {clue_id} {direction.value}"
 
 def get_location_id(clue_id: int, direction: Direction):
-    return clue_id + (MAX_N_CLUES * 10 if Direction == Direction.DOWN else 0);
+    return clue_id + (LOCATION_OFFSET if direction == Direction.DOWN else 0);
 
 location_table = {
     get_location_name(i, dir): get_location_id(i, dir) 
     for dir in [Direction.ACROSS, Direction.DOWN] 
     for i in range(0, MAX_N_CLUES)
-    } | {"VictoryLocation": MAX_N_CLUES * 100}
+    } | {"VictoryLocation": LOCATION_OFFSET * 2}
